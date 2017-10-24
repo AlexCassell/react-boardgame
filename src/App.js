a4d1d2ee1e8967c5499ee651e5a36314697b6882
Alex Cassell <alex@simulism.net>
2017-10-24 19:29:20 -0400

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App__header">
+          <img src={logo} className="App__logo" alt="logo" />
+          <h1 className="App__title">Welcome to Code Name: I do not know yet.</h1>
+          <div className="App__header__Login"><a href="somescript">Login</a>/<a href="somescript">Register</a></div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="gameBox">
+          <div class="gameBox__chatroom"></div>
+          <div class="gameBox__board"></div>
+        </div>
       </div>
     );
   }
